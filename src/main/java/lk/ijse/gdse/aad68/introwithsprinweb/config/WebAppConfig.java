709a39632ace42c3218bfe21e17979b0a3eb2716
ThanuraSilva <thanurasilva1989@gmail.com>
2024-08-11 13:25:45 +0530

    Test DemoController

--- src/main/java/lk/ijse/gdse/aad68/introwithsprinweb/config/WebAppConfig.java
+++ src/main/java/lk/ijse/gdse/aad68/introwithsprinweb/config/WebAppConfig.java
@@ -1,4 +1,9 @@
 package lk.ijse.gdse.aad68.introwithsprinweb.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.gdse.aad68.introwithsprinweb")
 public class WebAppConfig {
 }
--- src/main/java/lk/ijse/gdse/aad68/introwithsprinweb/controller/DemoController.java
+++ src/main/java/lk/ijse/gdse/aad68/introwithsprinweb/controller/DemoController.java
@@ -1,4 +1,13 @@
 package lk.ijse.gdse.aad68.introwithsprinweb.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/demo")
+@RestController
 public class DemoController {
+    @GetMapping(value = "/health")
+    public String healthCheck() {
+        return "DemoController run perfectly";
+    }
 }
